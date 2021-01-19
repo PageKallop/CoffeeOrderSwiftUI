5e16d1776c9042f24e01adb46fe855fd453904fb
PageKallop <PageMarie78@gmail.com>
2021-01-19 11:12:40 -0600

    Created request to push orders to api

---
+++ CoffeeOrderSwiftUI/Models/CreateOrderResponce.swift
@@ -0,0 +1,13 @@
+//
+//  CreateOrderResponce.swift
+//  CoffeeOrderSwiftUI
+//
+//  Created by Page Kallop on 1/19/21.
+//
+
+import Foundation
+
+struct CreateOrderResponce: Decodable {
+    let success: Bool 
+    
+}
--- CoffeeOrderSwiftUI/WebService.com.swift
+++ CoffeeOrderSwiftUI/WebService.com.swift
@@ -9,6 +9,35 @@ import Foundation
 
 class WebService {
     
+    func createCoffeeOder(order: OrderModel, completion: @escaping (CreateOrderResponce?) -> ()) {
+        
+        guard let url = URL(string: "https://island-bramble.glitch.me/orders") else {
+            fatalError("Invalid URL")
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-T")
+        request.httpBody = try? JSONEncoder().encode(order)
+        
+        URLSession.shared.dataTask(with: request) { data, responce, error in
+            guard let data = data, error == nil else {
+                
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            let createOrderResponce = try? JSONDecoder().decode(CreateOrderResponce.self, from: data)
+            DispatchQueue.main.async {
+                completion(createOrderResponce)
+            }
+            
+            
+        } .resume()
+    }
+    
     
     func getAllOrders(completion: @escaping ([OrderModel]?) -> ()) {
         
