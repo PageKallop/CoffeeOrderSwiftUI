4a020b793c112b16dd7f88dc4c764d93dcf92f9b
PageKallop <PageMarie78@gmail.com>
2021-01-19 12:59:40 -0600

    Set up coffee and size selection Added order button

--- CoffeeOrderSwiftUI/View/AddCoffeeOrderView.swift
+++ CoffeeOrderSwiftUI/View/AddCoffeeOrderView.swift
@@ -23,16 +23,27 @@ struct AddCoffeeOrderView: View {
                     Section(header: Text("Select Coffee").font(.body)) {
                         ForEach(addCoffeeOrderVM.coffeeList, id:\.name) { coffee in
                             
-                            HStack{
-                            Image(coffee.imageURL)
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                            Text(coffee.name)
-                            }
+                            CoffeCellView(coffee: coffee, selection: self.$addCoffeeOrderVM.coffeeName)
                             
                         }
                     }
+                    
+                    Section(header: Text("Select Coffee").font(.body), footer: Text("Total")) {
+                        Picker("",selection : self.$addCoffeeOrderVM.size) {
+                            Text("Small").tag("Small")
+                            Text("Medium")
+                                .tag("Medium")
+                            Text("Lage")
+                                .tag("Large")
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
                 }
+                HStack {
+                Button("Place Order") {
+                }
+                }.padding(EdgeInsets(top: 12, leading: 100, bottom: 12, trailing: 100))
+                .foregroundColor(Color.white)
+                    .background(Color(red: 46/255, green: 204/255, blue: 133/255))
             }
         }
     }
@@ -41,5 +52,29 @@ struct AddCoffeeOrderView: View {
 struct AddCoffeeOrderView_Previews: PreviewProvider {
     static var previews: some View {
         AddCoffeeOrderView()
+    }
+}
+
+struct CoffeCellView: View {
+   
+    
+    let coffee: CoffeeViewModel
+    
+    @Binding var selection: String
+    
+    var body: some View {
+        HStack{
+            Image(coffee.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+            Text(coffee.name)
+                .font(.title)
+                .padding([.leading], 20)
+            Spacer()
+            Image(systemName: self.selection == self.coffee.name ? "checkmark" : "").padding()
+        }.onTapGesture {
+            self.selection = self.coffee.name
+        }
     }
 }
